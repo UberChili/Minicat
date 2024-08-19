189ccefaa5fac6e0bb56a58f71fd2a68bb703444
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2024-08-19 16:03:40 -0600

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+
+pub struct Config {
+    pub files: Vec<String>,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
+
+        let mut result = vec![];
+
+        let args = &args[1..];
+        for arg in args {
+            result.push(arg);
+        }
+
+        let files = vec![];
+        Ok(Config { files })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {}
