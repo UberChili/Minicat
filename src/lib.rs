41958684f5237eb74608f64aecb5e30581194000
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2024-08-20 19:52:42 -0600

    made many changes, added the clap crate to try and better parse command line arguments. I have no idea what I'm doing

--- src/lib.rs
+++ src/lib.rs
@@ -29,3 +29,47 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+pub fn cat(files: Vec<String>) -> String {
+    let mut result: String = String::new();
+    for file in files {
+        if let Ok(contents) = fs::read_to_string(&file) {
+            result += &contents;
+        } else {
+            eprintln!("Unable to read file {file}");
+        }
+    }
+    result
+}
+
+//pub fn cat_with_numlines(files: Vec<String>) -> String {
+//
+//}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normally() {
+        let filenames = vec![
+            "1.txt".to_string(),
+            "2.txt".to_string(),
+            "3.txt".to_string(),
+        ];
+        let content_concatenated = "one\ntwo\nthree\n";
+
+        assert_eq!(content_concatenated, cat(filenames));
+    }
+
+    #[test]
+    fn with_numline() {
+        let filenames = vec![
+            "1.txt".to_string(),
+            "2.txt".to_string(),
+            "3.txt".to_string(),
+        ];
+        let content_concatenated = "1  one\n2  two\n3  three";
+        assert_eq!(content_concatenated, cat_with_numlines(filenames));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use std::{env, process};
+//use std::{env, process};
+//
+//use minicat::Config;
 
-use minicat::Config;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Show number lines
+    #[arg(short, long)]
+    number: bool,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    if let Err(e) = minicat::run(config) {
-        println!("Application error: {e}");
-        process::exit(1);
-    }
+    //let args: Vec<String> = env::args().collect();
+    //
+    //let config = Config::build(&args).unwrap_or_else(|err| {
+    //    eprintln!("Problem parsing arguments: {err}");
+    //    process::exit(1);
+    //});
+    //
+    //if let Err(e) = minicat::run(config) {
+    //    println!("Application error: {e}");
+    //    process::exit(1);
+    //}
+    let args = Args::parse();
+    println!("{}", args.number);
 }
